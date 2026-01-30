dd2642e068dcc143bf3ccc30e160bb3dc3b3ea7c
vittornatan8-prog <vittornatan8@gmail.com>
2026-01-30 19:22:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function toggleMenu() {
+  const menu = document.getElementById("menu-lateral");
+  menu.style.right = menu.style.right === "0px" ? "-260px" : "0px";
+}
+
+
+
+  
+  let lastScroll = 0;
+  const header = document.getElementById('header');
+  const delta = 8;      // sensibilidade (quanto maior, mais suave)
+  const headerHeight = header.offsetHeight;
+
+  window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset;
+
+    if (Math.abs(currentScroll - lastScroll) <= delta) return;
+
+    if (currentScroll > lastScroll && currentScroll > headerHeight + 40) {
+      header.classList.add('header-hide');
+    } else if (currentScroll < lastScroll) {
+      header.classList.remove('header-hide');
+    }
+
+    lastScroll = currentScroll;
+  });
+
+
+
